84d8997764dc7d78f99790f1f5ff40e84bf788d6
Josh Cohen <josh.cohen@slu.edu>
2023-05-05 01:51:01 -0500

    fixed logic for parsing and updated readme

--- README.md
+++ README.md
@@ -3,8 +3,14 @@ CSCI 3200 project - parsing a subset of HTML
 
 For this project, I will be parsing HTML, the simple markup language for creating webpages. Through the parsing program, users should be able to create their own basic html webpage through the commandline. 
 
-Tags to be included are mostly basic, such as: html, head, body, div, p, a, img, ul, ol, li, and h1-h4. Styling through in-line attributes is not possible as well as using the <style> tag (because that would require parsing CSS too).
+Tags to be included are mostly basic, such as: html, head, body, div, p, a, img, ul, ol, li, and h1 through h4. Styling through in-line attributes is not possible as well as using the <style> tag (because that would require parsing CSS too).
 
-Everything not commented out using // in the "my_grammar" block is the grammar used. In html.py, from lines 28-42, is the commented-out old method I used to implement the tag names... it did not exactly produce the tree I wanted.
+Everything not commented out using // in the "my_grammar" block is the grammar used. In html.py, starting at line 43, is the commented-out old method I used to implement the tag names... it did not exactly produce the tree I wanted.
 
-**To run the parser, simply run the html.py program**. It will ask for user input, in which you can type HTML tags. Users should start their input with <!DOCTYPE html> <html>, as that is the standard for HTML files. To end the input and kill the program, type "end". The *dominate* library (pip install dominate) is used to construct some HTML elements when they are read from user input.
+**To run the parser, simply run the html.py program**. It will ask for user input, in which you can type HTML tags. __Users should start their input with <!DOCTYPE html> <html> and end with </html>__, as that is the standard for HTML files. To end the input and gracefullly end the program, type "end".
+
+Sample programs are included in html.py as "sample_1" and "sample_2"
+
+Don't forget to close your tags!
+
+After a successful parsing of entered HTML, a .html file will save in the same directory as the html.py.
--- html.py
+++ html.py
@@ -1,29 +1,57 @@
 from lark import Lark
-import dominate
-from dominate.tags import *
 
 my_grammar = """
 
 start: "<!DOCTYPE html>" html -> start
 
-//tag: "<" content attribute* ">" (tag | text)*"</" content ">"
-
 html: "<html>" head* body* "</html>" -> html
 head: "<head>" title* div* "</head>" -> head
 body: "<body>" (div* | p* | h1* | h2* | h3* | h4* | a* | img* | ul* | ol* | li*)* "</body>" -> body
 div: "<div>" (div* | p* | h1* | h2* | h3* | h4* | a* | img* | ul* | ol* | li*)* "</div>" -> div
-title: "<title>" WORD* "</title>" -> title
-p: "<p>" WORD* "</p>" -> p
-h1: "<h1>" WORD* "</h1>" -> h1
-h2: "<h2>" WORD* "</h2>" -> h2
-h3: "<h3>" WORD* "</h3>" -> h3
-h4: "<h4>" WORD* "</h4>" -> h4
-a: "<a href=" ESCAPED_STRING ">" WORD* "</a>" -> a
+title: "<title>" (WORD* | SYMBOL | NUMBER*)* "</title>" -> title
+p: "<p>" (WORD* | SYMBOL | NUMBER*)* "</p>" -> p
+h1: "<h1>"(WORD* | SYMBOL | NUMBER*)* "</h1>" -> h1
+h2: "<h2>" (WORD* | SYMBOL | NUMBER*)* "</h2>" -> h2
+h3: "<h3>" (WORD* | SYMBOL | NUMBER*)* "</h3>" -> h3
+h4: "<h4>" (WORD* | SYMBOL | NUMBER*)* "</h4>" -> h4
+a: "<a href=" ESCAPED_STRING ">" (WORD* | SYMBOL | NUMBER*)* "</a>" -> a
 img: "<img src=" ESCAPED_STRING "/>" -> img
 ul: "<ul>" li* "</ul>" -> ul
 ol: "<ol>" li* "</ol>" -> ol
-li: "<li>" WORD* "</li>" -> li
+li: "<li>" (WORD* | SYMBOL | NUMBER*)* "</li>" -> li
 
+SYMBOL: "."
+        | ","
+        | "!"
+        | "?"
+        | "$"
+        | "#"
+        | "%"
+        | "("
+        | ")"
+        | "*"
+        | "&"
+        | "/"
+        | "+"
+        | "-"
+
+
+attribute: attribute_name "=" ESCAPED_STRING
+attribute_name: "style" -> style
+    | "id" -> id
+    | "class" -> class
+
+id: "id=" WORD
+class: "class=" WORD
+
+%import common.WS   
+%import common.WORD
+%import common.ESCAPED_STRING
+%import common.NUMBER
+
+%ignore WS 
+
+//tag: "<" content attribute* ">" (tag | text)*"</" content ">"
 
 // tag: html
 //     | head
@@ -41,113 +69,197 @@ li: "<li>" WORD* "</li>" -> li
 //     | ol
 //     | li
 
-attribute: attribute_name "=" ESCAPED_STRING
-attribute_name: "style" -> style
-    | "id" -> id
-    | "class" -> class
-
 // style: "style=" ESCAPED_STRING
 // style_option: "margin" | "width" | "height" | "padding" |
-
-id: "id=" WORD
-class: "class=" WORD
-
-%import common.WS   
-%import common.WORD
-%import common.ESCAPED_STRING
-%import common.NUMBER
-
-%ignore WS                     
 """
 
-test = """
-<!DOCTYPE html>
-<html>
-<head>
-<title>LOL</title>
-</head>
-<body>
-<h1>Hello</h1>
-<p>Goodbye</p>
-</body>
-</html>
-"""
-
-test2 = """
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-</body>
-</html>
-"""
 
 parser = Lark(my_grammar)
 
-doc = dominate.document('')
+file = 0
+content = 0
 
-print(parser.parse(test).pretty())
-
-def run_instruction(t):
+def run_instruction(t, name=0):
+    global content
     if t.data == 'start':
-        doc = dominate.document('your page')
-        print('checked start')
-        run_instruction(t.children)
+        global file
+        file = open(name + '.html', 'w')
+        file.write('<!DOCTYPE html> ')
+        if (len(t.children) > 0):
+            for child in t.children:
+                run_instruction(child)
+
     elif t.data == 'html':
-        print('checked html')
-        map(run_instruction, t.children)
+        file.write('<html> ')
+        if (len(t.children) > 0):
+            for child in t.children:
+                run_instruction(child)
+        file.write('</html>')
+        file.close()
+
     elif t.data == 'head':
-        print('checked head')
-        with doc.head:
-            map(run_instruction, t.children)
+        file.write('<head>')
+        if (len(t.children) > 0):
+            for child in t.children:
+                run_instruction(child)
+        file.write('</head>')
+
     elif t.data == 'body':
-        print('checked body')
-        with doc.body:
-            map(run_instruction, t.children)
+        file.write('<body> ')
+        if (len(t.children) > 0):
+            for child in t.children:
+                run_instruction(child)
+        file.write('</body>')
+
     elif t.data == 'div':
-        div(map(run_instruction, t.children))
+        file.write('<div>')
+        if (len(t.children) > 0):
+            for child in t.children:
+                run_instruction(child)
+        file.write('</div>')
+
     elif t.data == 'title':
-        title(t.children)
+        file.write('<title>' + t.children[0] + '</title>')
+
     elif t.data == 'p':
-        print('checked p')
-        p(t.children)
+        if len(t.children) > 1:
+            textList = []
+            for text in t.children:
+                textList.append(text)
+            content = ' '.join(textList)
+            file.write('<p>' + content + '</p>')
+        else:
+            file.write('<p>' + str(t.children[0].value) + '</p>')
+
     elif t.data == 'h1':
-        h1(t.children)
+        if len(t.children) > 1:
+            textList = []
+            for text in t.children:
+                textList.append(text)
+            content = ' '.join(textList)
+            file.write('<h1>' + content + '</h1>')
+        else:
+            file.write('<h1>' + str(t.children[0].value) + '</h1>')
+
     elif t.data == 'h2':
-        h2(t.children)
+        if len(t.children) > 1:
+            textList = []
+            for text in t.children:
+                textList.append(text)
+            content = ' '.join(textList)
+            file.write('<h2>' + content + '</h2>')
+        else:
+            file.write('<h2>' + str(t.children[0].value) + '</h2>')
+
     elif t.data == 'h3':
-        h3(t.children)
+        if len(t.children) > 1:
+            textList = []
+            for text in t.children:
+                textList.append(text)
+            content = ' '.join(textList)
+            file.write('<h3>' + content + '</h3>')
+        else:
+            file.write('<h3>' + str(t.children[0].value) + '</h3>')
+
     elif t.data == 'h4':
-        h4(t.children)
+        if len(t.children) > 1:
+            textList = []
+            for text in t.children:
+                textList.append(text)
+            content = ' '.join(textList)
+            file.write('<h4>' + content + '</h4>')
+        else:
+            file.write('<h4>' + str(t.children[0].value) + '</h4>')
+
     elif t.data == 'a':
-        a(href=t.children)
+        file.write('<a href=' + content + '>' + t.children[1].value + '</a>')
+
     elif t.data == 'img':
-        img(src=t.children)
+        file.write('<img src=' + t.children[0].value + '/>')
+
     elif t.data == 'ul':
-        ul(map(run_instruction, t.children))
+        file.write('<ul>')
+        if (len(t.children) > 0):
+            for child in t.children:
+                run_instruction(child)
+        file.write('</ul>')
+
     elif t.data == 'ol':
-        ol(map(run_instruction, t.children))
+        file.write('<ol>')
+        if (len(t.children) > 0):
+            for child in t.children:
+                run_instruction(child)
+        file.write('</ol>')
+
     elif t.data == 'li':
-        li(t.children)
+        if len(t.children) > 1:
+            textList = []
+            for text in t.children:
+                textList.append(text)
+            content = ' '.join(textList)
+            file.write('<li>' + content + '</li>')
+        else:
+            file.write('<li>' + str(t.children[0].value) + '</li>')
+
     else:
         raise SyntaxError('Unknown instruction: %s' % t.children)
 
 
-def run_html(program):
+def run_html(program, name):
     parse_tree = parser.parse(program)
-    print(parse_tree)
-    run_instruction(parse_tree)
+    run_instruction(parse_tree, name)
+
+sample_1 = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>This is the title!</title>
+</head>
+<body>
+<h1>This is h1!</h1>
+<p>This is p!</p>
+</body>
+</html>
+"""
+
+sample_2 = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sample 2</title>
+</head>
+<body>
+<h1>Welcome</h1>
+<h2>to</h2>
+<h3>HTML</h3>
+<p>Sample text</p>
+<ul>
+<li>List item 1</li>
+<li>List item 2</li>
+</ul>
+</body>
+</html>
+"""
 
 
 def main():
     while True:
-        sequence = input('Write some HTML: ')
+        name = input('Enter a file name (exclude .html): ')
+        sequence = input('Write some HTML (beginning with <!DOCTYPE html> <html>) or type "end" to close the program: ')
         if sequence == 'end':
             return 0
         try:
-            run_html(sequence)
-            print(doc)
+            run_html(sequence, name)
+            file = open(name + '.html', 'r')
+            print(' ')
+            print('******FOR DEBUG PURPOSES******')
+            print(file.readlines())
+
+            run_html(sample_1, 'sample_1')
+            print('SAMPLE PROGRAM #1 RAN AND SAVED AS SAMPLE_1.HTML')
+
+            run_html(sample_2, 'sample_2')
+            print('SAMPLE PROGRAM #2 RAN AND SAVED AS SAMPLE_2.HTML')
         except Exception as e:
             print(e)
 
