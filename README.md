55165aef362a8c7774611798b2a98e48b13851f1
Josh Cohen <josh.cohen@slu.edu>
2023-05-04 23:59:52 -0500

    updated grammar and readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 CSCI 3200 project - parsing a subset of HTML
+
+
+For this project, I will be parsing HTML, the simple markup language for creating webpages. Through the parsing program, users should be able to create their own basic html webpage through the commandline. 
+
+Tags to be included are mostly basic, such as: html, head, body, div, p, a, img, ul, ol, li, and h1-h4. Styling through in-line attributes is not possible as well as using the <style> tag (because that would require parsing CSS too).
+
+Everything not commented out using // in the "my_grammar" block is the grammar used. In html.py, from lines 28-42, is the commented-out old method I used to implement the tag names... it did not exactly produce the tree I wanted.
+
+**To run the parser, simply run the html.py program**. It will ask for user input, in which you can type HTML tags. Users should start their input with <!DOCTYPE html> <html>, as that is the standard for HTML files. To end the input and kill the program, type "end". The *dominate* library (pip install dominate) is used to construct some HTML elements when they are read from user input.
--- html.py
+++ html.py
@@ -4,42 +4,42 @@ from dominate.tags import *
 
 my_grammar = """
 
-start: "<!DOCTYPE html>" tag+
+start: "<!DOCTYPE html>" html -> start
 
 //tag: "<" content attribute* ">" (tag | text)*"</" content ">"
 
-html: "<html>" tag* "</html>"
-head: "<head>" tag* "</head>"
-body: "<body>" tag* "</body>"
-div: "<div>" tag* "</div>"
-title: "<title>" WORD* "</title>"
-p: "<p>" WORD* "</p>"
-h1: "<h1>" WORD* "</h1>"
-h2: "<h2>" WORD* "</h2>"
-h3: "<h3>" WORD* "</h3>"
-h4: "<h4>" WORD* "</h4>"
-a: "<a href=" ESCAPED_STRING ">" WORD* "</a>"
-img: "<img src=" ESCAPED_STRING "/>"
-ul: "<ul>" tag* "</ul>"
-ol: "<ol>" tag* "</ol>"
-li: "<li>" WORD* "</li>"
+html: "<html>" head* body* "</html>" -> html
+head: "<head>" title* div* "</head>" -> head
+body: "<body>" (div* | p* | h1* | h2* | h3* | h4* | a* | img* | ul* | ol* | li*)* "</body>" -> body
+div: "<div>" (div* | p* | h1* | h2* | h3* | h4* | a* | img* | ul* | ol* | li*)* "</div>" -> div
+title: "<title>" WORD* "</title>" -> title
+p: "<p>" WORD* "</p>" -> p
+h1: "<h1>" WORD* "</h1>" -> h1
+h2: "<h2>" WORD* "</h2>" -> h2
+h3: "<h3>" WORD* "</h3>" -> h3
+h4: "<h4>" WORD* "</h4>" -> h4
+a: "<a href=" ESCAPED_STRING ">" WORD* "</a>" -> a
+img: "<img src=" ESCAPED_STRING "/>" -> img
+ul: "<ul>" li* "</ul>" -> ul
+ol: "<ol>" li* "</ol>" -> ol
+li: "<li>" WORD* "</li>" -> li
 
 
-?tag: html
-    | head
-    | body
-    | div
-    | title
-    | p
-    | h1
-    | h2
-    | h3
-    | h4
-    | a
-    | img
-    | ul
-    | ol
-    | li
+// tag: html
+//     | head
+//     | body
+//     | div
+//     | title
+//     | p
+//     | h1
+//     | h2
+//     | h3
+//     | h4
+//     | a
+//     | img
+//     | ul
+//     | ol
+//     | li
 
 attribute: attribute_name "=" ESCAPED_STRING
 attribute_name: "style" -> style
@@ -73,47 +73,82 @@ test = """
 </html>
 """
 
+test2 = """
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+</body>
+</html>
+"""
+
 parser = Lark(my_grammar)
 
-doc = 0
+doc = dominate.document('')
 
 print(parser.parse(test).pretty())
 
 def run_instruction(t):
-    if t.children == 'start':
+    if t.data == 'start':
         doc = dominate.document('your page')
+        print('checked start')
         run_instruction(t.children)
-    elif t.children == 'html':
-        run_instruction(t.children)
-    elif t.children == 'head':
+    elif t.data == 'html':
+        print('checked html')
+        map(run_instruction, t.children)
+    elif t.data == 'head':
+        print('checked head')
         with doc.head:
-            run_instruction(t.children)
-    elif t.children == 'body':
+            map(run_instruction, t.children)
+    elif t.data == 'body':
+        print('checked body')
         with doc.body:
-            run_instruction(t.children)
-    elif t.children == 'title':
+            map(run_instruction, t.children)
+    elif t.data == 'div':
+        div(map(run_instruction, t.children))
+    elif t.data == 'title':
         title(t.children)
-    elif t.children == 'p':
+    elif t.data == 'p':
+        print('checked p')
         p(t.children)
-    elif t.children == 'h1':
+    elif t.data == 'h1':
         h1(t.children)
+    elif t.data == 'h2':
+        h2(t.children)
+    elif t.data == 'h3':
+        h3(t.children)
+    elif t.data == 'h4':
+        h4(t.children)
+    elif t.data == 'a':
+        a(href=t.children)
+    elif t.data == 'img':
+        img(src=t.children)
+    elif t.data == 'ul':
+        ul(map(run_instruction, t.children))
+    elif t.data == 'ol':
+        ol(map(run_instruction, t.children))
+    elif t.data == 'li':
+        li(t.children)
     else:
         raise SyntaxError('Unknown instruction: %s' % t.children)
 
 
 def run_html(program):
     parse_tree = parser.parse(program)
-    for inst in parse_tree.children:
-        run_instruction(inst)
+    print(parse_tree)
+    run_instruction(parse_tree)
 
 
 def main():
     while True:
         sequence = input('Write some HTML: ')
+        if sequence == 'end':
+            return 0
         try:
             run_html(sequence)
             print(doc)
         except Exception as e:
             print(e)
-    
-print(parser.parse(test))
+
+main()
